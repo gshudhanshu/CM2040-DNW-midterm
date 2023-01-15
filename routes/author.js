4de73bc68fd0c6fdb35206278fb93ec49e1fb08d
Shudhanshu Gunjal <33764308+gshudhanshu@users.noreply.github.com>
2023-01-15 10:10:19 +0530

    working on author's area

--- routes/author.js
+++ routes/author.js
@@ -3,8 +3,33 @@ const router = express.Router()
 const { query, param, body, validationResult } = require('express-validator')
 const db = require('../utils/sqlitePromises')
 
+const getBlogSettings = async () => {
+  const blog_settings = await db.get(
+    'SELECT * FROM blog_settings WHERE blog_id = 1'
+  )
+  return blog_settings
+}
+
 router.get('/', async (req, res) => {
-  res.render('author/index')
+  const blog_settings = await getBlogSettings()
+  const published_articles = await db.all(
+    "SELECT * FROM articles WHERE article_status='Published' ORDER BY article_updated_at DESC"
+  )
+  const draft_articles = await db.all(
+    "SELECT * FROM articles WHERE article_status='Draft' ORDER BY article_updated_at DESC"
+  )
+  res.render('author/index', {
+    blog_settings,
+    published_articles,
+    draft_articles,
+  })
+})
+
+router.get('/blog_settings', async (req, res) => {
+  const blog_settings = await getBlogSettings()
+  res.render('author/blog_settings', {
+    blog_settings,
+  })
 })
 
 module.exports = router
